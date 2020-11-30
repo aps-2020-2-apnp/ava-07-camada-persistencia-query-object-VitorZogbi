682b01676ad6be288317a759ae3f51e9b060f8d2
Vitor Zogbi <zogbivitor@gmail.com>
2020-11-30 16:01:13 -0300

    git config

--- webservice/schema.sql
+++ webservice/schema.sql
@@ -1,9 +1,16 @@
 DROP TABLE IF EXISTS usuarios;
+DROP TABLE IF EXISTS pets;
 
 CREATE TABLE usuarios (
   id        INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT,
   nome      VARCHAR(20) NOT NULL CHECK(LENGTH(nome) >= 2),
   sobrenome VARCHAR(20)     NULL
+);
+
+CREATE TABLE pets (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  nome VARCHAR(10),
+  responsavel INTEGER NOT NULL REFERENCES usuarios(id)
 );
 
 INSERT INTO usuarios (nome, sobrenome)
@@ -14,3 +21,15 @@ VALUES ('Eduardo', 'Silveira');
 
 INSERT INTO usuarios (nome, sobrenome)
 VALUES ('Renata', 'Lopes');
+
+INSERT INTO pets (nome, responsavel)
+VALUES ('Miau', 1);
+
+INSERT INTO pets (nome, responsavel)
+VALUES ('Snoop', 2);
+
+INSERT INTO pets (nome, responsavel)
+VALUES ('Mia', 3);
+
+INSERT INTO pets (nome, responsavel)
+VALUES ('Haley', 3);
